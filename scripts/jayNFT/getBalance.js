751723af364016520c656cbe518c4423b0213878
Joycejay17 <temitayojoy17@gmail.com>
2024-08-17 00:07:05 +0000

    resubmission for accurate bridging

--- scripts/jayNFT/getBalance.js
+++ scripts/jayNFT/getBalance.js
@@ -1,11 +1,11 @@
 const hre = require("hardhat");
 require("dotenv").config();
 
-const { ACCOUNT_ADDRESS, CONTRACT_ADDRESS } = process.env;
+const { ACCOUNT_ADDRESS, BRIDGE_ADDRESS } = process.env;
 
 async function main() {
   const JayNFT = await hre.ethers.getContractFactory("JayNFT");
-  const jayNFT = await JayNFT.attach(CONTRACT_ADDRESS);
+  const jayNFT = JayNFT.attach(BRIDGE_ADDRESS);
 
   const balance = await jayNFT.balanceOf(ACCOUNT_ADDRESS);
   console.log("Balance: ", balance.toString());
